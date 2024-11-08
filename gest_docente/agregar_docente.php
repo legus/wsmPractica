5925c20ee72140d81551c16e23913a62a972e13e
jutonito <juandavidariasarenas15@gmail.com>
2024-11-08 17:25:00 -0500

    arreglo en el collapse navbar del header

--- gest_docente/agregar_docente.php
+++ gest_docente/agregar_docente.php
@@ -34,6 +34,9 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         <nav class="navbar navbar-expand-lg bg-dark navbar-dark">
             <div class="container-fluid">
                 <a class="navbar-brand" href="index.php">inicio</a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
                 <div class="collapse navbar-collapse" id="navbarNav">
                     <ul class="navbar-nav">
                         <li class="nav-item">
